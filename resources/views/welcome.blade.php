8a3554d7a8f513136b6b2b4c0d16d27cc19f1a07
fredmeteor <fredpeter09899@gmail.com>
2024-09-04 20:43:19 +0300

    Added Php_codesniffer and phpcs.xml file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
- @section('title', 'Home Page - Lab Management System')
- @section('content')
- <div class="text-center">
- Welcome to the application
- </div>
- @endsection
+@section('title', 'Home Page - Lab Management System')
+@section('content')
+    <div class="text-center">
+        Welcome to the application
+    </div>
+@endsection
